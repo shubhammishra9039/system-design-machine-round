f114710440a58a7342002f8beaa8b78908d7fc3d
shubhammishra9039 <shubhammishra6595@gmail.com>
2025-12-15 23:16:59 +0530

    chore: set up React app and CSS modules

---
+++ Task-1 (Task Manager App)/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./css/app.module.css";
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.mainLeft}>hello</div>
+      <div className={styles.mainRight}></div>
+    </div>
+  );
+}
+
+export default App;
