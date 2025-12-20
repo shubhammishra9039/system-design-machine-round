5bacec3d8b78ba289d72c27a599f2f6c4199d387
shubhammishra9039 <shubhammishra6595@gmail.com>
2025-12-20 23:59:18 +0530

    Front End update

--- Task-1 (Task Manager App)/frontend/src/App.jsx
+++ Task-1 (Task Manager App)/frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import styles from "./css/app.module.css";
-
+import Img from "./assets/image.jpg"
 function App() {
   return (
     <div className={styles.container}>
       <div className={styles.mainLeft}>hello</div>
-      <div className={styles.mainRight}></div>
+      <div className={styles.mainRight}>
+        {/* <img 
+        src={Img}
+        className={styles.Image}
+        
+        /> */}
+      </div>
     </div>
   );
 }
